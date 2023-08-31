b456a06a604c0de2971c1be8ebb3cd08da0bddc5
David Zelaya <HP@PaviTallulah>
2023-08-30 19:02:53 -0600

    Login 3

--- src/Proyecto/Funciones.java
+++ src/Proyecto/Funciones.java
@@ -89,63 +89,6 @@ public class Funciones {
         return BuscarOponente(usuario, i + 1);
     }
 
-    public String[] ranking() {
-        String[] codigos = new String[tamaño];
-
-        for (int i = 0; i < tamaño; i++) {
-            if (!players[i].getUser().equals("                 ")) {
-                codigos[i] = players[i].getUser() + "                                                   " + players[i].getPuntos() + " puntos";
-            }
-        }
-        for (int i = 0; i < tamaño - 1; i++) {
-            for (int j = 0; j < tamaño - i - 1; j++) {
-                if (players[j].getPuntos() < players[j + 1].getPuntos()) {
-                    String temp = codigos[j];
-                    codigos[j] = codigos[j + 1];
-                    codigos[j + 1] = temp;
-                    Player tempPlayer = players[j];
-                    players[j] = players[j + 1];
-                    players[j + 1] = tempPlayer;
-                }
-            }
-        }
-
-        return codigos;
-    }
-
-    public void puntos(int i, String Ganador) {
-
-        Player jugador = players[i];
-
-        if (players[i] != null) {
-
-            if (jugador.getUser().equals(Ganador)) {
-                jugador.setpuntos(3);
-
-            }
-        }
-        if (i < tamaño) {
-            puntos(i + 1, Ganador);
-        }
-    }
-    
-      public void fechas(int i, String p1,String p2,String partida1,String partida2) {
-
-        Player jugador = players[i];
-
-        if (players[i] != null) {
-            if (jugador.getUser().equals(p1)) {
-                jugador.agregarFecha(partida1);
-            }   
-            if(jugador.getUser().equals(p2)){
-                jugador.agregarFecha(partida2);
-            }
-            
-        }
-        if (i < tamaño) {
-            fechas(i + 1, p1,p2,partida1,partida2);
-        }
-    }  
 
 }
 
